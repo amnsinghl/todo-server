b65113e2bf2ca0950cc96aa9e87991e1afe4aaa5
Aman Singhal <aman.si@riva.co>
2017-08-27 18:12:39 +0530

    add basic todo apis

--- todo/routes/index.js
+++ todo/routes/index.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
 
+todos={};
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/addTodo', function(req, res) {
+    res.end(addTodo(req.body.id, req.body.todo));
+});
+
+router.post('/getTodos', function(req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(getTodos(req.body.id)));
+});
+
+router.post('/removeTodo', function(req, res) {
+    res.end(removeTodo(req.body.id, req.body.todo));
+});
+
+function getTodos(id) {
+    tt =  todos[id];
+    if(!tt) todos[id] = {};
+    return todos[id];
+}
+
+function addTodo(id, todo) {
+    getTodos(id)[todo.id] = todo;
+}
+
+function removeTodo(id, todo) {
+    delete getTodos(id)[todo.id];
+}
 
 module.exports = router;
